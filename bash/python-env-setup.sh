366ac277564a64b8d6b868898d324312d494b439
Roberto Sero <roberto.sero42@gmail.com>
2020-01-05 20:45:23 +0100

    python-env-setup.sh

---
+++ bash/python-env-setup.sh
@@ -0,0 +1,16 @@
+VERSION="3.8.1"
+
+curl https://pyenv.run | bash
+echo 'export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+sudo apt-get install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
+pyenv install $VERSION
+pyenv global $VERSION
+pyenv virtualenv $VERSION tools
+pyenv activate tools
+pip install poetry
+pyenv deactivate
+pyenv global $VERSION tools
+python --version
+poetry --version
