b72e024eee73bc3f93486e7d0756b0a7a18093b6
Roberto Sero <roberto.sero42@gmail.com>
2020-01-05 21:32:32 +0100

    Update python-env-setup.sh

--- bash/python-env-setup.sh
+++ bash/python-env-setup.sh
@@ -4,7 +4,7 @@ VERSION="3.8.1"
 
 # if Ubuntu you may have to run
 # sudo apt-get install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
-curl https://pyenv.run | bash
+curl https://pyenv.run | bash || echo '1'
 echo 'export PATH="~/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
@@ -18,3 +18,8 @@ pyenv deactivate
 pyenv global $VERSION tools
 python --version
 poetry --version
+
+
+# to uninstall
+# rm -rf ~/.pyenv
+# and remove lines from .bashrc
